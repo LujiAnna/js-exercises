3e1f1ae8a0d68a3cd407300104f2f94b57aec6ad
Anna Ikoki <ikokianna@gmail.com>
2021-06-15 16:21:05 +0200

    Add calculator-one

--- 04-maths/01-calculator-one/script.js
+++ 04-maths/01-calculator-one/script.js
@@ -2,8 +2,8 @@
  *
  * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
  *
- * coded by leny@BeCode
- * started at 26/10/2018
+ * coded by Anna@BeCode
+ * started at 12/06/2021
  */
 
 // NOTE: don't focus on the existing code structure for now.
@@ -11,20 +11,33 @@
 
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
+    
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        let value1 = +document.getElementById("op-one").value;
+        let value2 = +document.getElementById("op-two").value;
+        console.log(value1 + value2)
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        let value1 = +document.getElementById("op-one").value;
+        let value2 = +document.getElementById("op-two").value;
+        console.log(value1 - value2)
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        let value1 = +document.getElementById("op-one").value;
+        let value2 = +document.getElementById("op-two").value;
+        console.log(value1 * value2)
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        let value1 = +document.getElementById("op-one").value;
+        let value2 = +document.getElementById("op-two").value;
+        console.log(value1 / value2)
     });
 })();
