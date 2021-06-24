e653fec00bcdcc12908aa65cce79b826cede5cc4
Anna Ikoki <ikokianna@gmail.com>
2021-06-24 09:45:35 +0200

    Display variable

--- 01-base/02-var-prompt/script.js
+++ 01-base/02-var-prompt/script.js
@@ -2,8 +2,8 @@
  *
  * /01-base/02-var-prompt/script.js - 1.2: var & prompt
  *
- * coded by leny@BeCode
- * started at 26/10/2018
+ * coded by Anna
+ * started at 24/06/2021
  */
 
 // NOTE: don't focus on the existing code structure for now.
@@ -11,6 +11,16 @@
 
 (function() {
 
-    // your code here
+    // var & prompt
+
+// Ask the name of the user
+
+// window.prompt() instructs the browser to display a dialog with an optional message
+ // prompting the user to input some text, and to wait until the user either submits
+  // the text or cancels the dialog.
+let name = prompt('What is your name?');
+
+// and display in the dialog box the following welcome message : Hello, [name] !.
+alert(`Hello, ${name} !`);
 
 })();
