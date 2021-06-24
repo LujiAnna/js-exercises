141703028a55d06f221ecb4607df0df48f9a76c2
Anna Ikoki <ikokianna@gmail.com>
2021-06-24 09:47:28 +0200

    Add logic

--- 01-base/03-prompt-if/script.js
+++ 01-base/03-prompt-if/script.js
@@ -2,8 +2,8 @@
  *
  * /01-base/03-prompt-if/script.js - 1.3: promp & if
  *
- * coded by leny@BeCode
- * started at 26/10/2018
+ * coded by Anna
+ * started at 24/06/2021
  */
 
 // NOTE: don't focus on the existing code structure for now.
@@ -11,6 +11,17 @@
 
 (function() {
 
-  // your code here
+  // prompt & if
+
+// Ask the user if he wants some cake. If yes, congratulate him/her; for any
+// other response, say: More cake for me then :p !
+let cake = prompt('Do you want some cake?');
+
+// yes
+// alert(`Congratulations !`);
+if (cake ==='yes' || cake === 'y' || cake === 'ya' || cake === 'sure')
+  alert(`Congratulations !`)
+else //else alert(`More cake for me then :p !`)
+alert(`More cake for me then :p !`);
 
 })();
