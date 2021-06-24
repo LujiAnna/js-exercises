b219b3e1216f28423b572ec46c89e2ef4e488d79
Anna Ikoki <ikokianna@gmail.com>
2021-06-24 09:43:28 +0200

    Display alert box

--- 01-base/01-var-alert/script.js
+++ 01-base/01-var-alert/script.js
@@ -2,8 +2,8 @@
  *
  * /01-base/01-var-alert/script.js - 1.1: var & alert
  *
- * coded by leny@BeCode
- * started at 26/10/2018
+ * coded by Anna
+ * started at 24/06/2021
  */
 
 // NOTE: don't focus on the existing code structure for now.
@@ -11,6 +11,13 @@
 
 (function() {
 
-    // your code here
+       // var & alert
+    // create a variable
+   let test = 'Hello, World!';
+
+   // Display the variable in in a dialog box (alert box).
+   // window.alert() instructs the browser to display a dialog with an optional message,
+   // and to wait until the user dismisses the dialog.
+   alert(test);
 
 })();
