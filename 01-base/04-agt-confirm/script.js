283fea6f2dcd9b6d76283722397c9e4d315436e4
Anna Ikoki <ikokianna@gmail.com>
2021-06-24 09:51:25 +0200

    Delete old unstyled folders

--- 01-base/04-agt-confirm/script.js
+++ 01-base/04-agt-confirm/script.js
@@ -2,8 +2,8 @@
  *
  * /01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
  *
- * coded by leny@BeCode
- * started at 26/10/2018
+ * coded by Anna
+ * started at 24/06/2021
  */
 
 // NOTE: don't focus on the existing code structure for now.
@@ -11,6 +11,28 @@
 
 (function() {
 
-    // your code here
+// age/gender/town with confirmation
+
+// Ask three questions to the user: his/her Age, Gender, and the Town where he/she lives.
+ // Display the collected responses in a dialog box and ask to confirm.
+ // If not confirmed, re-run the process.
+
+// TODO: Ask user input
+let userAge = prompt('What is your age');
+console.log('userAge');
+console.log(userAge);
+
+// TODO: do same for gender and location
+let userGender = prompt('What is your gender');
+let userTown = prompt('Which town do you live');
+
+// TODO: Ask for confirmation
+let infoCorrect = confirm('Is this' + userAge + 'your name?');
+// TODO: Optional: If not confirmed, ask again
+if(infoCorrect === false){
+  // TODO: rerun
+   userAge = prompt('What is your age');
+   infoCorrect = confirm('Is the info correct?');
+}
 
 })();
