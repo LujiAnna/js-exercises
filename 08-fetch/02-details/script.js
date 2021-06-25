f4ccfda6d4fd445a9c791b00c019770e963e9d4c
Anna Ikoki <ikokianna@gmail.com>
2021-06-25 10:05:21 +0200

    Show template -version1

--- 08-fetch/02-details/script.js
+++ 08-fetch/02-details/script.js
@@ -1,31 +1,16 @@
-/* becode/javascript
- *
- * coded by leny@BeCode
- * started at 12/05/2019
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (() => {
-    // your code here
+  
     // TODO: 1. click on the button, get the id from the form
     let inputId = document.getElementById('hero-id');
-    console.log(inputId);
+    // console.log(inputId);
     
-    btnId = document.getElementById('run');
-    btnId.addEventListener('click', grab);
+    // btnId = document.getElementById('run');
+    // btnId.addEventListener('click', grab);
 
     let targetId = document.getElementById('target');
-    console.log(targetId);
-    // TODO: error- Returns null when querySelect    
-    let name = document.querySelector('.name');
-    console.log(name);
-    let alterEgo = document.querySelector('.alter-ego');
-    console.log(alterEgo);
-    let powers = document.querySelector('.powers');
-    console.log(powers);
-
+    // console.log(targetId);
+  
 
     function grab() {
         console.log(inputId.value);
@@ -39,21 +24,40 @@
           let array = data.heroes;
         //   console.log(array[inputId.value-1].name);
         //   OR
-        console.log(data.heroes[inputId.value-1].name);
-let ourHero = inputId.value;
-       // TODO: 3. display it in the tag whose id is "target". 
-       targetId.innerHTML = data.heroes[inputId.value-1].name;
-       name.innerHTML = data.heroes[inputId.value-1].name;
+        //    console.log(data.heroes[inputId.value-1].name);
+            let ourHero = inputId.value;
+            // TODO: 3. display it in the tag whose id is "target". 
+            targetId.innerHTML = data.heroes[inputId.value-1].name;
+            name.innerHTML = data.heroes[inputId.value-1].name;
         })
-
     }
 
     // TODO: 4. Use the tag template to reproduce a suitable html structure.
-// TODO: error
-    (function(){
+    // TODO: error
+
+    btnId = document.getElementById('run');
+    btnId.addEventListener('click', showTemplateDisplay);
+
+      // TODO: error- Returns null when querySelect  
+      const content = document.querySelector(".content");
+      const template = document.getElementById("tpl-hero");
+  
+  const clone = template.content.firstElementChild.cloneNode(true);
+//   secondChance.addEventListener("click", clickHandler);
+// container.appendChild(clone);
+
+      let name = clone.querySelector('.name');
+      console.log(name); //strong.name
+
+      let alterEgo = clone.querySelector('.alter-ego');
+      console.log(alterEgo); //em.alter-ego
+      let powers = clone.querySelector('.powers');
+      console.log(powers); //p.powers
+
+    function showTemplateDisplay(){
         fetch('http://127.0.0.1:5500/_shared/api.json')
             .then((response)=>{
-               console.log(response); 
+            //    console.log(response); 
                 return response.json();
                 // console.log(response.json())
             })
@@ -61,12 +65,13 @@ let ourHero = inputId.value;
 
 for (let index = 0; index < data.heroes.length; index++) {
     console.log(data.heroes[index].name);
-    name.innerHTML = data.heroes[index].name;
-    alterEgo.innerHTML = data.heroes[index].alterEgo;
-    powers.innerHTML = data.heroes[index].abilities;
+    name.textContent = data.heroes[index].name;
+    alterEgo.textContent = data.heroes[index].alterEgo;
+    // powers.textContent = data.heroes[index].abilities;
    }
-            })
+   content.appendChild(clone);
 
-})
+            })
+}
 
 })();
